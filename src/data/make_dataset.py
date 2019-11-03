cc1dd012845e5f09eb057946a50181b8f9067225
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2019-11-03 19:31:37 +0100

    Resolved merge conflict

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! /bin/bash
 # -*- coding: utf-8 -*-
 import logging
@@ -28,8 +27,9 @@ def read_in_data(path_bitcoin_df='data/raw/1_training_data_sets/1_bitcoin_price_
     tuple (df, df, df)
         df_bitcoin, df, df_test
     """
-    df_bitcoin = pd.read_csv(path_bitcoin_df, encoding="ISO-8859-1", delimiter=';')
-    df = pd.read_csv(path_training_df,encoding="ISO-8859-1")
+    df_bitcoin = pd.read_csv(
+        path_bitcoin_df, encoding="ISO-8859-1", delimiter=';')
+    df = pd.read_csv(path_training_df, encoding="ISO-8859-1")
 
     df_test = pd.read_csv(path_test_df, encoding="ISO-8859-1")
 
@@ -87,93 +87,3 @@ def get_preprocessed_datasets(path_bitcoin_df='data/raw/1_training_data_sets/1_b
     logger.info('making final data sets from raw data')
     df_bitcoin, df, df_test = read_in_data()
     return df_bitcoin, df, df_test
-=======
-#! /bin/bash
-# -*- coding: utf-8 -*-
-import logging
-import pandas as pd
-import numpy as np
-
-logger = logging.getLogger(__name__)
-
-
-def read_in_data(path_bitcoin_df='data/raw/1_training_data_sets/1_bitcoin_price_data_set.csv',
-                 path_training_df='data/raw/1_training_data_sets/1_training_data.csv',
-                 path_test_df='data/raw/2_classification_data.csv'):
-    """Function to read in data
-
-    Parameters
-    ----------
-    path_bitcoin_df : str, optional
-        Path to bitcoin set, by default 'data/raw/1_training_data_sets/1_bitcoin_price_data_set.csv'
-    path_training_df : str, optional
-        Path to training set, by default 'data/raw/1_training_data_sets/1_training_data.csv'
-    path_test_df : str, optional
-        Path to training set, by default 'data/raw/2_classification_data.csv'
-
-
-    Returns
-    -------
-    tuple (df, df, df)
-        df_bitcoin, df, df_test
-    """
-    df_bitcoin = pd.read_csv(path_bitcoin_df, encoding="ISO-8859-1", delimiter=';')
-    df = pd.read_csv(path_training_df,encoding="ISO-8859-1")
-
-    df_test = pd.read_csv(path_test_df, encoding="ISO-8859-1")
-
-    logger.info("Shape of df_bitcoin: {}".format(df_bitcoin.shape))
-    logger.info("Shape of df: {}".format(df.shape))
-    return df_bitcoin, df, df_test
-
-
-def clean_data(df_in):
-    """This function cleans data and removes errornous data.
-
-    Parameters
-    ----------
-    df_in : DataFrame
-        DataFrame to be cleaned
-
-    ReturnsFeatureEngineering
-    -------
-    DataFrame
-        Cleaned DataFrame
-    """
-    df = df_in.copy()
-
-    return df
-
-
-def preprocess_dtypes(df_in):
-    """This function preprocessed and changes all columns to the right dtype.
-
-    Parameters
-    ----------
-    df_in : DataFrame
-        Original DataFrame
-
-    Returns
-    -------
-    DataFrame
-        Preprocessed DataFrame
-    """
-
-    # Copy DataFrame -> If not you edit the original one in the memory
-    df = df_in.copy()
-
-    return df
-
-
-def get_preprocessed_datasets(path_bitcoin_df='data/raw/1_training_data_sets/1_bitcoin_price_data_set.csv',
-                              path_training_df='data/raw/1_training_data_sets/1_training_data.csv',
-                              path_test_df='data/raw/2_classification_data.csv'):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-    logger.info('making final data sets from raw data')
-    df_bitcoin, df, df_test = read_in_data()
-    return df_bitcoin, df, df_test
->>>>>>> 08ede1a0365feeba9ccdd5929537f2d1a20eaa57
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! /bin/bash
 # -*- coding: utf-8 -*-
 
@@ -67,24 +66,23 @@ class FeatureEngineering(object):
             self.df_features, df[['OBS_ID', column]])
         assert column in self.df_features.columns, "No {column} in df_features!"
 
-
-    def _nan_values_to_string(self, df,column):
+    def _nan_values_to_string(self, df, column):
         df.loc[df[column].isna(), column] = "NAN"
         return df
 
-    def normalize_categorical_feature(self,df,  column):
+    def normalize_categorical_feature(self, df,  column):
         labels = self.le.fit_transform(df[column])
-        return labels 
-    
+        return labels
+
     def _add_category(self):
         logger.info("Adding categories_0")
         df_copy = self.df.copy()
         df_copy = self._nan_values_to_string(df_copy, 'categories_0')
         labels = self.normalize_categorical_feature(df_copy, "categories_0")
-        df_copy = df_copy.assign(labels_categories_0 = labels)
+        df_copy = df_copy.assign(labels_categories_0=labels)
 
         self._add_column_to_data_frame(df_copy, "labels_categories_0")
-        
+
     def _add_transaction_count(self):
         """This function adds the feature transaction count to the feature dataset. 
         It will add the feature for train and test set.
@@ -143,103 +141,3 @@ class FeatureEngineering(object):
         self._add_transaction_count()
         self._add_holder_count()
         self._add_category()
-=======
-#! /bin/bash
-# -*- coding: utf-8 -*-
-
-import logging
-import pandas as pd
-import numpy as np
-from sklearn import preprocessing
-
-logger = logging.getLogger(__name__)
-log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-
-class FeatureEngineering(object):
-
-    # Constructor
-    def __init__(self, df, df_bitcoin, df_test):
-        """Constructor for class FeatureEngineering
-
-        Parameters
-        ----------
-        df : DataFrame
-            Train DataFrame
-        df_bitcoin : DataFrame
-            Bitcoin DataFrame
-        df_test : DataFrame
-            Test DataFrame
-        """
-        self.df = df
-        self.df_bitcoin = df_bitcoin
-        self.df_test = df_test
-        self.le =  preprocessing.LabelEncoder()
-
-        # Create empty dataframe to add features
-        self.df_features_train = self.df[['OBS_ID','categories_0', 'success']].copy()
-        self.df_features_test = self.df_test[['OBS_ID']].copy()
-
-
-    def normalize_categorical_feature(self, column):
-        self.le.fit(self.df['categories_0'])
-        self.le.transform(self.df['categories_0']) 
-
-    def denormalize_categorical_feature(self, column):
-        print(column)
-
-
-    def _add_transaction_count(self, train=True):
-        """This function adds the feature transaction count to the feature dataset. 
-        It will add the feature for train and test set.
-
-        Parameters
-        ----------
-        train : bool, optional
-            Trainset or testset?, by default True
-        """
-        logger.info("Adding transaction count for {}".format(
-            "train" if train else "test"))
-        normalize_categorical_feature(self, "categories_0")
-
-        # Copy DataFrame
-        df_copy = self.df.copy() if train else self.df_test.copy()
-
-        # Fill NA values with mean
-        df_copy.transaction_count.fillna(
-            df_copy.transaction_count.mean(), inplace=True)
-        
-
-        if train:
-            self.df_features_train = pd.merge(
-                self.df_features_train, df_copy[['OBS_ID', 'categories_0' 'transaction_count']])
-            assert 'transaction_count' in self.df_features_train.columns, "No transaction count in df_features!"
-        else:
-            self.df_features_test = pd.merge(
-                self.df_features_test, df_copy[['OBS_ID', 'categories_0','transaction_count']])
-            assert 'transaction_count' in self.df_features_test.columns, "No transaction count in df_features!"
-
-    def get_X_y(self):
-        """This function returns X_train, y_train and X_test.
-        These are not the splits for training! This is just for preprocessing both datasets.
-
-        Returns
-        -------
-        DataFrames
-            X_train, y_train, X_test
-        """
-        self.X_train = self.df_features_train.drop(
-            ['success', 'OBS_ID'], axis=1)
-        self.y_train = self.df_features_train.loc[:, 'success']
-
-        self.X_test = self.df_features_test
-
-        return self.X_train, self.y_train, self.X_test
-
-    def construct_features(self):
-        """This function is the pipeline for adding all features to the dataset
-        """
-        self._add_transaction_count(train=True)
-        self._add_transaction_count(train=False)
->>>>>>> 08ede1a0365feeba9ccdd5929537f2d1a20eaa57
