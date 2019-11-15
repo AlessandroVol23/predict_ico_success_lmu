868971a81a242f4e09de13e70fa4cff84aa03f96
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2019-11-15 17:58:20 +0100

    Added makefile and lbm model draft

--- src/models/light_gbm.py
+++ src/models/light_gbm.py
@@ -4,7 +4,12 @@
 import logging
 from sklearn.model_selection import train_test_split
 import lightgbm as lgb
+from lightgbm import LGBMClassifier
 from sklearn.metrics import matthews_corrcoef
+from sklearn.model_selection import KFold
+import numpy as np
+from src.models.utils import read_feature_data
+from sklearn.metrics import roc_auc_score
 
 logger = logging.getLogger(__name__)
 log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -13,7 +18,7 @@ logging.basicConfig(level=logging.INFO, format=log_fmt)
 
 class LightGbmModel(object):
 
-    def __init__(self, X, y):
+    def __init__(self):
         """Constructor for class LightGbmModel
 
         Parameters
@@ -23,16 +28,45 @@ class LightGbmModel(object):
         y : DataFrame
             y_train DataFrame from build_features.py
         """
-        self.X = X
-        self.y = y
+        self.X_train, self.y_train, self.X_test = read_feature_data()
 
-    def preprocess(self):
-        """Preprocess data for model.
-        At the moment just creating a test / validation split.
+    def cross_validation(self):
+        """Cross validation
         """
-        # Create train / test split
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.y, test_size=0.2, random_state=42)
+        # Modeling
+        folds = KFold(n_splits=5, shuffle=True, random_state=123)
+        oof_preds = np.zeros(self.X_train.shape[0])
+        sub_preds = np.zeros(self.X_test.shape[0])
+        for n_fold, (trn_idx, val_idx) in enumerate(folds.split(self.X_train)):
+            trn_x, trn_y = self.X_train.iloc[trn_idx], self.y_train.iloc[trn_idx]
+            val_x, val_y = self.X_train.iloc[val_idx], self.y_train.iloc[val_idx]
+
+            clf = LGBMClassifier(
+                n_estimators=2000,
+                learning_rate=0.1,
+                num_leaves=123,
+                colsample_bytree=.8,
+                subsample=.9,
+                max_depth=15,
+                reg_alpha=.1,
+                reg_lambda=.1,
+                min_split_gain=.01,
+                min_child_weight=2
+            )
+
+            clf.fit(trn_x, trn_y,
+                    eval_set=[(trn_x, trn_y), (val_x, val_y)],
+                    eval_metric='auc', verbose=250, early_stopping_rounds=150
+                    )
+
+            oof_preds[val_idx] = clf.predict_proba(
+                val_x, num_iteration=clf.best_iteration_)[:, 1]
+            sub_preds += clf.predict_proba(test[features], num_iteration=clf.best_iteration_)[
+                :, 1] / folds.n_splits
+
+            print('Fold %2d AUC : %.6f' %
+                  (n_fold + 1, roc_auc_score(val_y, oof_preds[val_idx])))
+            del clf, trn_x, trn_y, val_x, val_y
 
     def train(self):
         """Train lightgbm model with some harcoded parameters
@@ -74,3 +108,13 @@ class LightGbmModel(object):
         # Print mcc
         logger.info("MCC is; {}".format(
             matthews_corrcoef(self.y_test, classes_preds)))
+
+
+def main():
+    model = LightGbmModel()
+    model.cross_validation()
+    pass
+
+
+if __name__ == "__main__":
+    main()
