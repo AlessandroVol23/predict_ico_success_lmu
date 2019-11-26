f89277716407723c80e8d23121905ffb0ad746a3
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2019-11-26 14:00:45 +0100

    First draft of model chain

---
+++ src/models/model_chain.py
@@ -0,0 +1,23 @@
+import click
+from src.utils import read_feature_meta
+from src.models import build_model
+
+
+@click.command()
+@click.argument('feature_set')
+def main(feature_set):
+    feature_set_meta = read_feature_meta()
+    keys = feature_set_meta.keys()
+
+    for key in keys:
+        print(key)
+
+    for key in keys:
+        print("FOR KEY: {}".format(key))
+        build_model.main([key])
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
