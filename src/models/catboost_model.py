28504b000588c2c7ce6373a39d2e0e436b6f3ca6
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2019-12-12 19:59:16 +0100

    changed catboost params

--- src/models/catboost_model.py
+++ src/models/catboost_model.py
@@ -11,40 +11,44 @@ from catboost import CatBoostClassifier, Pool
 class CatBoostModel(BaseModel):
 
     def __init__(self):
-        self. hyperparam = {
-            'iterations':500,
-             'depth':2,
-             'eval_metric':'MCC',
-             'loss_function':'Logloss',
-             'verbose':True
+        self.hyperparam = {
+            'iterations': 2000,
+          #  'depth': 8,
+            'eval_metric': 'MCC',
+            'loss_function': 'Logloss',
+          #  'learning_rate': 0.1,
+            'use_best_model': True,
+            'early_stopping_rounds': 100,
+            'task_type': 'GPU',
+            'logging_level':'Verbose'
         }
-        self.model =CatBoostClassifier(
+        self.model = CatBoostClassifier(
             **self.hyperparam
         )
-
 
     def get_name(self):
         return "catboost"
-    
+
     def get_params(self):
         return self.hyperparam
 
-    def get_model(self, reinitialize = False):
-       
+    def get_model(self, reinitialize=False):
         if (reinitialize):
-            self.model =CatBoostClassifier(
-            **self.hyperparam
-        )
+            self.model = CatBoostClassifier(
+                **self.hyperparam
+            )
         return self.model
-    def fit(self, trn_x, trn_y,val_x, val_y ):
+
+    def fit(self, trn_x, trn_y, val_x, val_y):
         self.model.fit(trn_x, trn_y,
-                        eval_set=[(trn_x, trn_y), (val_x, val_y)],
-                        )
-    def predict_proba(self,oof_preds,sub_preds,X_test,folds,val_idx,val_x ):
+                       eval_set=[(val_x, val_y)],
+                       )
+
+    def predict_proba(self, oof_preds, sub_preds, X_test, folds, val_idx, val_x):
         oof_preds[val_idx] = self.model.predict_proba(
             val_x)[:, 1]
         sub_preds += self.model.predict_proba(X_test)[
-            :, 1] / folds.n_splits
+                     :, 1] / folds.n_splits
 
         oof_pred_abs = oof_preds.round()
 
