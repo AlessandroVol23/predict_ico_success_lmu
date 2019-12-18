eda289ef6b99f87f2f1f1d2195fc253f29795fac
Florian Fuchs <fuchsfl@me.com>
2019-12-18 12:03:22 +0100

    added links_hompage_available as binary data to raw dataset

--- src/features/get_http_codes.py
+++ src/features/get_http_codes.py
@@ -4,17 +4,20 @@ import pandas as pd
 from multiprocessing.pool import ThreadPool, Pool
 import numpy as np
 import re
-import requests  
+import requests
+from tqdm import tqdm
 
-url_reg_ex =  regex = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-            r'localhost|' #localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-            r'(?::\d+)?' # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+url_reg_ex = regex = re.compile(
+    r'^(?:http|ftp)s?://'  # http:// or https://
+    # domain...
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+    r'localhost|'  # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-def _make_valid_url( url):
+
+def _make_valid_url(url):
 
     result = re.match(url_reg_ex, url)
 
@@ -29,17 +32,24 @@ def _check_http_status_code(url):
         return 0
     url = _make_valid_url(url)
     try:
-        r = requests.head(url)
+        r = requests.head(url, timeout=3)
         if r.status_code >= 200 and r.status_code < 300:
             return 1
         else:
             return 0
     except requests.ConnectionError:
         return 0
+    except requests.Timeout:
+        return 0
+    except:
+        return 0
 
-def _transform_link_binary( column="links_homepage", na_strategy ="set:NAN"):
+
+def transform_link_binary(column="links_homepage", na_strategy="set:NAN"):
     df_bitcoin, df, df_test, df_gem_btc_usd = read_processed_data()
     df_copy = df.copy()
+    df_raw = pd.read_csv('data/raw/1_training_data_sets/1_training_data.csv', encoding="ISO-8859-1")
+
     results = []
     if na_strategy.find(":") != -1:
         strat = na_strategy.split(":")
@@ -49,8 +59,10 @@ def _transform_link_binary( column="links_homepage", na_strategy ="set:NAN"):
             raise ValueError("Unrecognized command strategy for {column}")
 
     with Pool(processes=20) as pool:
-        results = pool.map(_check_http_status_code, df_copy[column])
+        results = list(tqdm(pool.imap(_check_http_status_code,
+                                      df_copy[column]), total=len(df_copy[column])))
 
-    df_copy["links_homepage_available"] = results
+    df_raw["links_homepage_available"] = results
 
-    df_copy.to_csv('data/raw/1_training_data_sets/1_training_data.csv')
+    df_raw.to_csv(
+        'data/raw/1_training_data_sets/1_training_data_new.csv',   encoding="ISO-8859-1")
