3eb96a8b803117297a503724797b893d317d407e
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2019-12-19 09:32:23 +0100

    some adjustements on shap exaplainer

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def read_feature_meta(random = False):
     if random: 
@@ -7,3 +8,7 @@ def read_feature_meta(random = False):
     else:
         with open("data/features/feature_set_meta/feature_set_meta.json") as f:
             return (json.load(f))
+
+def create_folder(folder_to_save):
+    if not os.path.exists(folder_to_save):
+        os.makedirs(folder_to_save)
